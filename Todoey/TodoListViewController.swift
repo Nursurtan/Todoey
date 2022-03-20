4124801a84febd65727532855b1e8f6222ae11f6
Nursurtan <arapovnur23@gmail.com>
2022-03-20 22:03:25 +0700

    Add Subclassing a UITableViewController, TableView Delegate Methods and Accessories

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,14 +11,40 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       print(itemArray)
+        print(itemArray)
+    }
+    
+    // Mark - TableView Datasourse Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    // Mark - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     
-
-
 }
 

f98e7b3ce0a8b10fb3801e1fa6687e08a9af1985
Nursurtan <arapovnur23@gmail.com>
2022-03-23 20:49:01 +0700

    Added creation of a custom Data Model

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by нурсултан арапов on 3/22/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}

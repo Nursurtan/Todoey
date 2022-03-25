b19bec186db0bdaac4306f65887720fc8267046e
Nursurtan <arapovnur23@gmail.com>
2022-03-25 21:31:25 +0700

    Added Configure Core Data

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreData
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,28 +23,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        
-        
-     
-        
+    
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+       
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-     print("applicationDidEnterBackground")
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+      
+        let container = NSPersistentContainer(name: "")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+               
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-     print("applicationWillTerminate")
-    }
 
 
 }

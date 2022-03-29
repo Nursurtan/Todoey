1e6f83d262bfc59b1cfaacc8f99fbf1ab8292e2a
Nursurtan <arapovnur23@gmail.com>
2022-03-29 08:54:48 +0700

    Added "How to Create Relationship Graphs in Core Data"

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by нурсултан арапов on 3/29/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    //MARK: - TableView Datasourse Methods
+    
+    //MARK: - Data Manipulation Methods
+    
+    // MARK: - Add New Categories
+
+  
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
+   
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+    
+    
+    
+    
+}
--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -152,7 +152,5 @@ extension TodoListViewController: UISearchBarDelegate {
         }
         
         
-        
-        
     }
 }
